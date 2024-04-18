0f29146a23f9e02d8f8692698452b2a29a902eb7
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2024-04-18 23:20:47 +0530

    Update 3.dart

--- Dart/PR_1/3.dart
+++ Dart/PR_1/3.dart
@@ -1,22 +1,52 @@
+import 'dart:io';
+
 void main() {
-  List a = [
-    [4,5,6,],
-    [9,7,8]
-  ];
-  List b = [
-    [5, 5, 5],
-    [5, 5, 5]
-  ];
-  List c = [
-    [0, 0, 0],
-    [0, 0, 0]
-  ];
+  stdout.write("Enter size of in Array : ");
+  int size = int.parse(stdin.readLineSync()!);
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j <3;j++) {
-      c[i][j] = a[i][j] + b[i][j];
-    }
+  List<int> a = List.generate(size, (index) => index);
+
+  for (var i = 0; i < size; i++) {
+    stdout.write("Enter element of $i : ");
+    int val = int.parse(stdin.readLineSync()!);
+    a[i] = val;
   }
+  print("==============");
+  print("1. Insert");
+  print("2. Delet");
+  print("3. Update");
+  print("4. View");
+  print("5. Exit");
+  print("==============");
+  stdout.write("Enter your choice : ");
+  int choice = int.parse(stdin.readLineSync()!);
 
-  print("$c");
+  switch (choice) {
+    case 1:
+      stdout.write('Enter The value in insert :');
+      int i = int.parse(stdin.readLineSync()!);
+      stdout.write('Enter value : ');
+      choice = int.parse(stdin.readLineSync()!);
+      a.insert(i, choice);
+      break;
+    case 2:
+      stdout.write('Enter The value for remove : ');
+      choice = int.parse(stdin.readLineSync()!);
+      a.remove(choice);
+      break;
+
+    case 3:
+      stdout.write('Enter The index of update value : ');
+      int i = int.parse(stdin.readLineSync()!);
+      stdout.write('Enter value : ');
+      choice = int.parse(stdin.readLineSync()!);
+      a[i] = choice;
+      break;
+
+    case 4:
+      print('List : $a');
+
+    default:
+      print('Invalid choice!!');
+  }
 }

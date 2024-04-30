dc40fbf73c10209eed128fdf2b8331ad1cbd57cf
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2024-04-30 11:39:13 +0530

    Add files via upload

---
+++ Dart/6.3/6.4/1.dart
@@ -0,0 +1,16 @@
+class Student{
+  late int roll;
+  late String name;
+  late String course;
+
+  factory Student.from({required int roll,required String name,required String course}){
+  return Student(
+    id : data['id'];
+  );
+  }
+}
+
+
+void main(){
+
+}
